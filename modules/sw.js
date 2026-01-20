8a3b2c264c3977211c608c16b3d1c7a37d43fc8f
Luca Gargaz <gargazlucaalexandru@gmail.com>
2026-01-20 10:12:43 +0200

    Update sw.js

--- modules/sw.js
+++ modules/sw.js
@@ -6,11 +6,22 @@ const URLS_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(URLS_TO_CACHE))
-      .catch(err => console.error("Cache error", err))
-  );
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    for (const url of URLS_TO_CACHE) {
+      try {
+        const res = await fetch(url, {cache: "no-store"});
+        console.log("SW install fetch", url, res.status);
+        if (res.ok) {
+          await cache.put(url, res.clone());
+        } else {
+          console.warn("SW install: non-OK response, skipping", url, res.status);
+        }
+      } catch (err) {
+        console.error("SW install: fetch failed for", url, err);
+      }
+    }
+  })());
 });
 
 self.addEventListener("fetch", event => {
