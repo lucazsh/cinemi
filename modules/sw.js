06e9f355a1dc2d1e31f30fb88ffbab6190a86d25
Luca Gargaz <gargazlucaalexandru@gmail.com>
2026-01-20 09:57:11 +0200

    Update sw.js

--- modules/sw.js
+++ modules/sw.js
@@ -1,9 +1,15 @@
 const CACHE_NAME = "pwa-cache-v1";
-const URLS_TO_CACHE = ["/", "/index.html"];
+const URLS_TO_CACHE = [
+  "/cinemi/index.html",
+  "/cinemi/icons/icon-192.png",
+  "/cinemi/icons/icon-512.png"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .catch(err => console.error("Cache error", err))
   );
 });
 
