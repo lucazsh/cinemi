68abd3c16f7de843912c0a22598eccbc6b86c9ae
Luca Gargaz <gargazlucaalexandru@gmail.com>
2026-02-03 12:27:11 +0200

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'https://mark-sword-foo-coupon.trycloudflare.com';
+const SERVER_URL = 'https://rugs-refused-photographers-sam.trycloudflare.com';
 const baseUrl = SERVER_URL ? SERVER_URL.replace(/\/$/, '') : '';
 const ngrokHeaders = { 'ngrok-skip-browser-warning': 'true' };
 
@@ -73,4 +73,5 @@ function updateAllTimestamps() {
     });
 }
 setInterval(updateAllTimestamps, 30000);
+
 
