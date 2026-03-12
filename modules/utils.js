0dc8c6c9418eec426164e93b8fbeb4e5a87c534c
Luca Gargaz <gargazlucaalexandru@gmail.com>
2026-03-12 21:20:46 +0200

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'https://reservation-aluminum-see-wifi.trycloudflare.com';
+const SERVER_URL = 'https://miss-thin-sat-engaging.trycloudflare.com';
 const baseUrl = SERVER_URL ? SERVER_URL.replace(/\/$/, '') : '';
 const ngrokHeaders = { 'ngrok-skip-browser-warning': 'true' };
 
@@ -79,6 +79,7 @@ function updateAllTimestamps() {
     });
 }
 setInterval(updateAllTimestamps, 30000);
+
 
 
 
