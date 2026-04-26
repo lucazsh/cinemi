87461ee254ea22ecfebdded7cf8a50e8878ac5b3
Luca Gargaz <gargazlucaalexandru@gmail.com>
2026-04-26 18:38:29 +0300

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'https://smithsonian-performs-stick-vernon.trycloudflare.com';
+const SERVER_URL = 'https://recovery-favourites-leasing-eagle.trycloudflare.com';
 const baseUrl = SERVER_URL ? SERVER_URL.replace(/\/$/, '') : '';
 const ngrokHeaders = { 'ngrok-skip-browser-warning': 'true' };
 
