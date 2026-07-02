f0c6192cfb4a0a99246b13c166f5e020b6155a3e
Luca Gargaz <gargazlucaalexandru@gmail.com>
2026-07-02 15:49:40 +0300

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'https://sara-keep-statistical-arts.trycloudflare.com';
+const SERVER_URL = 'https://judgment-pubs-promotion-interpreted.trycloudflare.com';
 const baseUrl = SERVER_URL ? SERVER_URL.replace(/\/$/, '') : '';
 const ngrokHeaders = { 'ngrok-skip-browser-warning': 'true' };
 
