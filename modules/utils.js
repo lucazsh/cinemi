d3c77e57849ab98f73c659416d1027629c5fd794
Luca Gargaz <gargazlucaalexandru@gmail.com>
2026-07-05 23:51:54 +0300

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'https://particularly-bath-cir-oasis.trycloudflare.com';
+const SERVER_URL = 'https://wagner-apache-ala-running.trycloudflare.com';
 const baseUrl = SERVER_URL ? SERVER_URL.replace(/\/$/, '') : '';
 const ngrokHeaders = { 'ngrok-skip-browser-warning': 'true' };
 
