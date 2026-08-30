50c767fed748d148ea777c7816c548541f26104f
Luca Gargaz <gargazlucaalexandru@gmail.com>
2026-08-30 08:49:08 +0200

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'https://spent-tips-properties-anything.trycloudflare.com';
+const SERVER_URL = 'https://unto-seas-front-strain.trycloudflare.com';
 const baseUrl = SERVER_URL ? SERVER_URL.replace(/\/$/, '') : '';
 const ngrokHeaders = { 'ngrok-skip-browser-warning': 'true' };
 
